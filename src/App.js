ce9af047aad3327ce4e7207acd1b2bab2c18e94c
shubham-chawla <chawlashubham007@gmail.com>
2018-10-03 02:15:04 +0530

    month picker added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Calendar from 'react-calendar';
 import './App.css';
+import { format } from 'date-fns/esm';
 
+
+const Month = _ => (
+  <Calendar
+    onChange={(val) => console.log(format(val, 'MMMM YYYY'))}
+    value={new Date()}
+    minDate={new Date()}
+    maxDetail={'year'}
+  />
+);
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Month />
       </div>
     );
   }
